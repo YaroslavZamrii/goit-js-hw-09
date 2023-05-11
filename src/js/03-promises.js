18258fba36eb1694f2b33e210d24ea0e65f72235
YaroslavZamrii <119421294+YaroslavZamrii@users.noreply.github.com>
2023-05-11 15:14:34 +0300

    Update 03-promises.js

--- src/js/03-promises.js
+++ src/js/03-promises.js
@@ -1,3 +1,10 @@
+const refs = {
+  form: document.querySelector('form.form'),
+  delay: document.querySelector('[name="delay"]'),
+  step: document.querySelector('[name="step"]'),
+  amount: document.querySelector('[name="amount"]'),
+};
+
 function createPromise(position, delay) {
   const shouldResolve = Math.random() > 0.3;
   if (shouldResolve) {
