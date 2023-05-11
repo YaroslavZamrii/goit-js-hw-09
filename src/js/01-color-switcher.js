0460dc78ef9a8d677344e1cfe5092fa5b7acdc8f
YaroslavZamrii <119421294+YaroslavZamrii@users.noreply.github.com>
2023-05-11 13:33:17 +0300

    timer

--- src/js/01-color-switcher.js
+++ src/js/01-color-switcher.js
@@ -1,1 +1,32 @@
+const CHANGE_COLOR_DELAY = 1000;
+let timerId = null;
 
+const refs = {
+  startBtnEl: document.querySelector('button[data-start]'),
+  stopBtnEl: document.querySelector('button[data-stop]'),
+  bodyEl: document.querySelector('body'),
+};
+
+refs.startBtnEl.addEventListener('click', onStartColorSwitcher);
+refs.stopBtnEl.addEventListener('click', onStopColorSwitcher);
+refs.stopBtnEl.disabled = true;
+
+function onStartColorSwitcher() {
+  refs.startBtnEl.disabled = true;
+  refs.stopBtnEl.disabled = false;
+  timerId = setInterval(() => {
+    refs.bodyEl.style.backgroundColor = getRandomHexColor();
+  }, CHANGE_COLOR_DELAY);
+}
+
+function onStopColorSwitcher() {
+  refs.stopBtnEl.disabled = true;
+  refs.startBtnEl.disabled = false;
+  clearInterval(timerId);
+}
+
+function getRandomHexColor() {
+  return `#${Math.floor(Math.random() * 16777215)
+    .toString(16)
+    .padStart(6, 0)}`;
+}
