b63f5b4c411545ef03b2976777338b7f32be5e6d
YaroslavZamrii <119421294+YaroslavZamrii@users.noreply.github.com>
2023-05-13 17:24:07 +0000

    Deploying to gh-pages from @ YaroslavZamrii/goit-js-hw-09@b9c851ca3b311a5c5f92a70268f3e8a72b12cdaf 🚀

--- 03-promises.07a684fa.js
+++
@@ -1,2 +0,0 @@
-document.querySelector("form.form"),document.querySelector('[name="delay"]'),document.querySelector('[name="step"]'),document.querySelector('[name="amount"]');
-//# sourceMappingURL=03-promises.07a684fa.js.map
--- 03-promises.fd69c7aa.js
+++
@@ -1,2 +0,0 @@
-document.querySelector("form.form"),document.querySelector('[name="delay"]'),document.querySelector('[name="step"]'),document.querySelector('[name="amount"]');
-//# sourceMappingURL=03-promises.fd69c7aa.js.map
